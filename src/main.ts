546763d3b39cfb22ec6d7e45e181582feeb4bf66
Alexey Pyzhianov <alex.pyzhianov@gmail.com>
2020-08-20 19:29:30 +0300

    A very lively version

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 const cellSize = 10
 const fieldSide = 64
+const seed = 0.45
 
 const canvas = document.getElementById("life") as HTMLCanvasElement
 canvas.width = fieldSide * cellSize
@@ -8,16 +9,11 @@ canvas.height = fieldSide * cellSize
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
 ctx.fillStyle = "#000000"
 
-const field = new Array(Math.pow(fieldSide, 2)).fill(0)
-randomize(field)
+const field = new Array(Math.pow(fieldSide, 2))
+    .fill(0)
+    .map(() => Math.round(Math.random() - seed))
 
-function randomize(arry: any[]) {
-    arry.forEach((_, index) => {
-        arry[index] = Math.round(Math.random())
-    })
-}
-
-function mainLoop() {
+function mainLoop(): void {
     ctx.clearRect(0, 0, fieldSide * cellSize, fieldSide * cellSize)
 
     field.forEach((cell, index) => {
@@ -28,6 +24,48 @@ function mainLoop() {
             ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize)
         }
     })
+
+    evolve(field)
 }
 
 setInterval(mainLoop, 500)
+
+// Rules:
+// Any live cell with two or three live neighbours survives.
+// Any dead cell with three live neighbours becomes a live cell.
+// All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+function evolve(field: number[]): void {
+    field.forEach((cell, index) => {
+        const dead = cell === 0
+        const neighbors = countLivingNeighbors(field, index)
+
+        if (dead && neighbors === 3) {
+            // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+            field[index] = 1
+        } else if (!dead && neighbors < 2) {
+            // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+            field[index] = 0
+        } else if (!dead && neighbors > 3) {
+            // Any live cell with more than three live neighbours dies, as if by overpopulation.
+            field[index] = 0
+        }
+    })
+}
+
+//   [i - row - 1] [i - row] [i - row + 1]
+//   [i - 1]       [i]       [i + 1]
+//   [i + row - 1] [i + row] [i + row + 1]
+function countLivingNeighbors(field: number[], cellIndex: number): number {
+    const indices = [
+        cellIndex - fieldSide - 1,
+        cellIndex - fieldSide,
+        cellIndex - fieldSide + 1,
+        cellIndex - 1,
+        cellIndex + 1,
+        cellIndex + fieldSide - 1,
+        cellIndex + fieldSide,
+        cellIndex + fieldSide + 1,
+    ]
+
+    return indices.reduce((acc, index) => acc + field[index], 0)
+}
